2d25e387d77c83aed0f5276fd1e91f83e0c85d0c
givenone <givenone1@gmail.com>
2019-10-23 14:34:52 +0900

    added calibration

--- SLhaircapture/cvStructuredLight.cpp
+++ SLhaircapture/cvStructuredLight.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
 	}
 
 	delete[] proj_gray_codes;
-
+// Codes below are for the 
 //	IplImage** proj_gray_codes_S = NULL;
 //	generateGrayCodes_S(width /*sl_params->proj_w*/, height /*sl_params->proj_h*/, proj_gray_codes_S, 
 //		gray_ncols, gray_nrows, gray_colshift, gray_rowshift, 
@@ -59,8 +59,7 @@ int main(int argc, char* argv[])
 	struct slParams sl_params; //	configuration
 	struct slCalib sl_calib; //	calibration
 	
-	readConfiguration(NULL, &sl_params);
-	
+	readConfiguration(NULL, &sl_params);	
 		// Allocate storage for calibration parameters.
 	
 	int cam_nelems                  = sl_params.cam_w*sl_params.cam_h;
@@ -68,8 +67,8 @@ int main(int argc, char* argv[])
     sl_calib.cam_intrinsic_calib    = false;
 	sl_calib.proj_intrinsic_calib   = false;
 	sl_calib.procam_extrinsic_calib = false;
-	sl_calib.cam_intrinsic          = cvCreateMat(3,3,CV_32FC1);
-	sl_calib.cam_distortion         = cvCreateMat(5,1,CV_32FC1);
+	sl_calib.cam_intrinsic          = cvCreateMat(3,3,CV_32FC1); 
+	sl_calib.cam_distortion         = cvCreateMat(5,1,CV_32FC1); 
 	sl_calib.cam_extrinsic          = cvCreateMat(2, 3, CV_32FC1);
 	sl_calib.proj_intrinsic         = cvCreateMat(3, 3, CV_32FC1);
 	sl_calib.proj_distortion        = cvCreateMat(5, 1, CV_32FC1);
@@ -80,57 +79,45 @@ int main(int argc, char* argv[])
 	sl_calib.proj_rays              = cvCreateMat(3, proj_nelems, CV_32FC1);
 	sl_calib.proj_column_planes     = cvCreateMat(sl_params.proj_w, 4, CV_32FC1);
 	sl_calib.proj_row_planes        = cvCreateMat(sl_params.proj_h, 4, CV_32FC1);
+
+	double cam_intrinsic[3][3] = { {6799.891745995248, 0, 1684.663925292664}, {0, 6819.065266606978, 897.2080419838242}, {0, 0, 1}};
+	double cam_distortion[5] = { -0.1081234940885747, 1.019777262201034, -0.01496156727613678, 0.007012687213256888, 0};
+	cvInitMatHeader(sl_calib.cam_intrinsic, 3, 3, CV_32FC1, cam_intrinsic);
+	cvInitMatHeader(sl_calib.cam_distortion, 5, 1, CV_32FC1, cam_distortion);
+
+	double cam_rotation[9] = {-0.9465088474336322, -0.02181055404986991, -0.3219399034942621, 
+		0.03029318697689328, -0.9993127627683679, -0.02136176469851316, 
+		-0.3212527424826753, -0.0299716849758128, 0.9465190825055085 
+		};
+	double cam_translation[3] = {208.4992551331917, -9.241636763329311, 63.17988709955341};
+
+	CvMat* r = cvCreateMat(1, 3, CV_32FC1);
+	CvMat* t = cvCreateMat(1, 3, CV_32FC1);
+	CvMat* R = cvCreateMat(3, 3, CV_64F);
+	cvInitMatHeader(R, 3, 3, CV_32FC1, cam_rotation);
+	cvInitMatHeader(r, 1, 3, CV_32FC1, cam_translation);
+	cvRodrigues2(R, r, NULL); // rotation vector
+	for(int i=0; i<3; i++) CV_MAT_ELEM(sl_calib.cam_extrinsic, float, 0, i) = (float)cvmGet(r, 0, i);
+	for(int i=0; i<3; i++) CV_MAT_ELEM(sl_calib.cam_extrinsic, float, 1, i) = (float)cvmGet(t, 0, i);
+
+	double proj_intrinsic[3][3] = { {1583.136672775252, 0, 516.2688867516777},
+		{0, 2105.254975139322, 519.7798700367449},
+		{0, 0, 1} };
+	double proj_distortion[5] = { 0.04757942626277912, -0.4940169148610814, 0.01733058285795064, 0.01007747086733519, 0 };
+
+	cvInitMatHeader(sl_calib.proj_intrinsic, 3, 3, CV_32FC1, proj_intrinsic);
+	cvInitMatHeader(sl_calib.proj_distortion, 5, 1, CV_32FC1, proj_distortion);
+
+	double proj_rotation[9] = { 1, 0, 0, 0, 1, 0, 0, 0, 1};
+	double proj_translation[3] = {0, 0, 0};
+	cvInitMatHeader(R, 3, 3, CV_32FC1, proj_rotation);
+	cvInitMatHeader(r, 1, 3, CV_32FC1, proj_translation);
+	cvRodrigues2(R, r, NULL); // rotation vector
+	for(int i=0; i<3; i++) CV_MAT_ELEM(sl_calib.proj_extrinsic, float, 0, i) = (float)cvmGet(r, 0, i);
+	for(int i=0; i<3; i++) CV_MAT_ELEM(sl_calib.proj_extrinsic, float, 1, i) = (float)cvmGet(t, 0, i);
+
 /*
-** 프로젝터가 원점 ! -> no extrinsic value
-
-Camera Calib:
-
-- reprojection error: 2.12458
-
-- Intrinsic Param:
-
-[6799.891745995248, 0, 1684.663925292664;
-
-0, 6819.065266606978, 897.2080419838242;
-
-0, 0, 1]
-
-- distance Coefficients: [-0.1081234940885747, 1.019777262201034, -0.01496156727613678, 0.007012687213256888, 0]
-
-Extrinsic Param:
-
-- reprojection error: 1.7807
-
-- R:
-
-[-0.9465088474336322, -0.02181055404986991, -0.3219399034942621;
-
-0.03029318697689328, -0.9993127627683679, -0.02136176469851316;
-
--0.3212527424826753, -0.0299716849758128, 0.9465190825055085]
-
-- T:
-
-[208.4992551331917;
-
--9.241636763329311;
-
-63.17988709955341]
-
-Projector Calib:
-
-- reprojection error: 0.553558
-
-- Intrinsic Param:
-
-[1583.136672775252, 0, 516.2688867516777;
-
-0, 2105.254975139322, 519.7798700367449;
-
-0, 0, 1]
-
-- distance Coefficients: [0.04757942626277912, -0.4940169148610814, 0.01733058285795064, 0.01007747086733519, 0]
-
+	프로젝터가 원점 ! -> no extrinsic value
 */
 	// Initialize background model.
 
@@ -149,7 +136,7 @@ Projector Calib:
 	for(int i=0; i<2*(gray_ncols+gray_nrows+1); i++)
 	{
 		sprintf(temp, "./Face_6mp_01/800x600/%02d.png", i);
-		cam_gray_codes[i] = cvLoadImage(temp)); //// TODO : read png files & need to modify array size
+		cam_gray_codes[i] = cvLoadImage(temp); //// TODO : read png files & need to modify array size
 	}		
 
 	// Decode gray codes
@@ -170,7 +157,7 @@ Projector Calib:
 	CvMat *points  = cvCreateMat(3, sl_params.cam_h*sl_params.cam_w, CV_32FC1);
 	CvMat *colors  = cvCreateMat(3, sl_params.cam_h*sl_params.cam_w, CV_32FC1);
 	CvMat *mask    = cvCreateMat(1, sl_params.cam_h*sl_params.cam_w, CV_32FC1);
-	reconstructStructuredLight(sl_params, sl_calib, 
+	reconstructStructuredLight(&sl_params, &sl_calib, 
 							   cam_gray_codes[0],
 		                       gray_decoded_cols, gray_decoded_rows, sl_calib.background_mask,
 							   points, colors, sl_calib.background_depth_map, mask);
